c92652d9326af3d37a05a1e32902ca03423da645
Steve Quinn <steve@sq.net.au>
2024-05-29 18:02:44 +1000

    Initial commit of vite react project with dot.

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import dotSVG from '/dot.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <a href="https://dotdoing.com">
+          <img src={dotSVG} className="logo dot" alt="dot doing" />
+        </a>
+     </div>
+    </>
+  )
+}
+
+export default App
